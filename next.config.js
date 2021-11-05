08ae97dc8b3e9a2b691a4b93c2e60e274c5f7ae6
ewno92 <77653755+ewno92@users.noreply.github.com>
2021-11-05 12:21:18 -0700

    added paypal button in cart page

--- next.config.js
+++ next.config.js
@@ -7,5 +7,7 @@ module.exports = {
     ACCESS_TOKEN_SECRET: process.env.ACCESS_TOKEN_SECRET,
     REFRESH_TOKEN_SECRET: process.env.REFRESH_TOKEN_SECRET,
     JWT_SECRET: process.env.JWT_SECRET,
+    SANDBOX_CLIENT_ID: process.env.SANDBOX_CLIENT_ID,
+    SANDBOX_CLIENT_SECRET: process.env.SANDBOX_CLIENT_SECRET,
   },
 };
